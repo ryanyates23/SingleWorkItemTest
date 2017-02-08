9d674e961cdc62f28056b9515110bd822ba338b7
Ryan Yates <ryanyates23@gmail.com>
2017-02-08 16:00:24 +0000

    Initial Commit

---
+++ SingleWorkItemTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SingleWorkItemTest
+//
+//  Created by Ryan Yates on 08/02/2017.
+//  Copyright © 2017 Ryan Yates. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
